14efa8a91a520e6a82e1e64f46e9978c8bef9657
Mark C Allen <mark@markcallen.com>
2023-10-14 20:37:46 -0700

    Fixed deprecated warning

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,20 +3,20 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { DVCProvider } from '@devcycle/devcycle-react-sdk';
+import { DevCycleProvider } from '@devcycle/devcycle-react-sdk';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <DVCProvider
-      config={{
-        sdkKey: process.env.NEXT_PUBLIC_DVC_SDK_KEY || ''
-      }}
-    >
-      <UserProvider>
+    <UserProvider>
+      <DevCycleProvider
+        config={{
+          sdkKey: process.env.NEXT_PUBLIC_DVC_SDK_KEY || ''
+        }}
+      >
         <CacheProvider>
           <ChakraProvider>{children}</ChakraProvider>
         </CacheProvider>
-      </UserProvider>
-    </DVCProvider>
+      </DevCycleProvider>
+    </UserProvider>
   );
 }
